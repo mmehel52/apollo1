219e9c7b480824de68df5c7bda1aa26b1fb8f4e5
Mustafa Mehel <109292336+mmehel52@users.noreply.github.com>
2025-09-16 22:09:56 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-cron.schedule("8 * * * *", async () => {
+cron.schedule("15 * * * *", async () => {
   Logger.info("Scrapping app cron");
   await main();
   Logger.success("Scrapped app");
